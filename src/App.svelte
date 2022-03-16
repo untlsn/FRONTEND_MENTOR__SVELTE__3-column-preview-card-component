d7384cab36c55041b2c4e3d3d3465cd0c6240955
Filip Skoczeń <philipskoczen@gmail.com>
2022-03-16 11:25:19 +0100

    Create

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,41 @@
-<main class="flex items-center justify-center min-h-screen text-center">
-  <div>
-    <h1 class="text-4xl">Hello World!</h1>
-    <p>from svelte</p>
+<script lang="ts">
+import Column from '~/components/organisms/Column.svelte';
+import luxury from '~/assets/icon-luxury.svg';
+import sedans from '~/assets/icon-sedans.svg';
+import suvs from '~/assets/icon-suvs.svg';
+</script>
+
+<main class="flex items-center justify-center min-h-screen">
+  <div class="flex rounded-xl overflow-hidden">
+    <Column
+      color="#E28525"
+      name="sedans"
+      text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+    >
+      <img slot="img" src={sedans} alt="sedans car">
+    </Column>
+    <Column
+      color="#016972"
+      name="suvs"
+      text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+    >
+      <img slot="img" src={suvs} alt="suvs car">
+    </Column>
+    <Column
+      color="#00403F"
+      name="luxury"
+      text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+    >
+      <img slot="img" src={luxury} alt="luxury car">
+    </Column>
   </div>
 </main>
+
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
+  main {
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+  }
+</style>
---
+++ src/components/organisms/Column.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+export let color: string;
+export let name: string;
+export let text: string;
+</script>
+
+
+<article style="--bg: {color}" class="w-80 text-white p-12 space-y-10">
+  <slot name="img" />
+  <h1 class="text-4xl uppercase">{name}</h1>
+  <p class="pb-8 opacity-70">{text}</p>
+  <button
+    class="px-8 py-3 border-2 border-white rounded-full bg-white hover:(bg-transparent text-white)"
+    on:click={() => alert(`Learn more about ${name}`)}
+  >
+    Learn more
+  </button>
+</article>
+
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&display=swap');
+  h1 {
+    font-family: 'Big Shoulders Display', cursive;
+    font-weight: 700;
+  }
+  article {
+    --bg: '';
+    background-color: var(--bg);
+  }
+  button {
+    color: var(--bg);
+  }
+</style>
